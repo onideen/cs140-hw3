e18d3985ea70f6db3f2ada8a2ec43369f7264550
Vegar Engen <vegar.engen@gmail.com>
2013-01-31 13:00:29 -0800

    make random a float between 0 and 1

--- harness/submit.c
+++ harness/submit.c
@@ -43,7 +43,7 @@ void gennbody(double** s, double** v, double* m, int n) {
 	double dist, theta;
 	srand(time(NULL));
 	for(i = 0; i <10;i++){
-		printf("%i \n", rand());
+		printf("%f \n", (float)rand());
 
 	}
 	for (i = 0; i < n; i++) {
