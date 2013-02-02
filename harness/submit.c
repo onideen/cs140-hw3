81484082312dfd12765994573ab1c40b1f894350
Vegar Engen <vegar.engen@gmail.com>
2013-02-02 15:41:54 -0800

    Fix?

--- harness/submit.c
+++ harness/submit.c
@@ -186,16 +186,7 @@ void nbody(double** s, double** v, double* m, int n, int iter, int timestep) {
 	free(currentplanets);
 	free(acceleration);
 
-	for (p = 0; p < nprocs; p++) {
-		if (myrank == p) {
-			for (i = 0; i < size; i++) {
-				fprintf(stderr, OUTPUT_BODY, s[i][0], s[i][1], s[i][2], v[i][0], v[i][1], v[i][2], m[i]);
-			}
-		}
-		MPI_Barrier(MPI_COMM_WORLD);
-	}
-
-//	printInOrder(myrank, nprocs, size, s, v, m);
+	printInOrder(myrank, nprocs, size, s, v, m);
 	
 }
 
