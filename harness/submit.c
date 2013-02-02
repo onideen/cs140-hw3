db4a1b3b7fce2642e171a7834be401c6eebf2c5f
Vegar Engen <vegar.engen@gmail.com>
2013-02-02 15:37:16 -0800

    Fix?

--- harness/submit.c
+++ harness/submit.c
@@ -188,7 +188,7 @@ void nbody(double** s, double** v, double* m, int n, int iter, int timestep) {
 
 	for (p = 0; p < nprocs; p++) {
 		if (rank == p) {
-			for (i = 0; i < nbodies; i++) {
+			for (i = 0; i < size; i++) {
 				fprintf(stderr, OUTPUT_BODY, s[i][0], s[i][1], s[i][2], v[i][0], v[i][1], v[i][2], m[i]);
 			}
 		}
