dd445697457e724987647d857946f892e6519b72
Vegar Engen <vegar.engen@gmail.com>
2013-02-02 16:29:36 -0800

    Hva med naa

--- harness/submit.c
+++ harness/submit.c
@@ -210,7 +210,7 @@ double norm(double * x){
 void resetMatrix(double** matrix) {
 	int i, j, len, len0;
 	len = sizeof(matrix);
-	len0 = sizeof(matrix[0])/sizeof(matrix[0][0]);
+	len0 = sizeof(matrix[0]);
 	printf("Len: %d\nLen0: %d\n", len, len0);
 	for (i = 0; i < len; i++)
 		for (j = 0; j < len0; j++)
